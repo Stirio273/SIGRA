fb7c4db15c3581fd9892192ca0d4d67f71cc571c
Tsiory Itokiana <tsioryitokiana273@gmail.com>
2026-09-07 22:45:31 +0300

    Synchronize AD and local database user

--- Services/AD/ProvisioningClaimsTransformation.cs
+++ Services/AD/ProvisioningClaimsTransformation.cs
@@ -14,17 +14,49 @@ public class ProvisioningClaimsTransformation : IClaimsTransformation
 
     public async Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
     {
-        var samAccountName = principal.Identity!.Name!.Split('\\')[1];
-        var existe = await _db.Utilisateurs.AnyAsync(u => u.IdentifiantAd == samAccountName); 
+        if (!principal.IsInRole(@"DEV\SIGRA-Autorises"))
+            return principal;
 
-        if(!existe)
+        var samAccountName = principal.Identity!.Name!.Split('\\')[1];
+        var user = await _db.Utilisateurs
+            .Include(u => u.IdRoleNavigation)
+            .FirstOrDefaultAsync(u => u.IdentifiantAd == samAccountName);
+
+        if (user == null)
         {
-            _db.Utilisateurs.Add(new Utilisateur
+            var defaultRole = await _db.Roles
+                .FirstOrDefaultAsync(r => r.Libelle == "Technicien");
+                // ?? await _db.Roles
+                //     .FirstOrDefaultAsync(r => r.Libelle == "Consultant")
+                // ?? await _db.Roles.FirstOrDefaultAsync();
+
+            if (defaultRole == null)
+                return principal;
+
+            user = new Utilisateur
             {
                 IdentifiantAd = samAccountName,
-                Nom = principal.Identity.Name               
-            });
+                Nom = principal.Identity.Name,
+                Prenom = samAccountName,
+                Email = $"{samAccountName}@gmail.com",
+                Actif = true,
+                DateSynchronisation = DateTime.UtcNow,
+                IdRole = defaultRole.IdRole,
+                UserGuid = Guid.NewGuid()
+            };
+
+            _db.Utilisateurs.Add(user);
             await _db.SaveChangesAsync();
+
+            user = await _db.Utilisateurs
+                .Include(u => u.IdRoleNavigation)
+                .FirstAsync(u => u.IdentifiantAd == samAccountName);
+        }
+
+        if (user.IdRoleNavigation != null)
+        {
+            var identity = (ClaimsIdentity)principal.Identity!;
+            identity.AddClaim(new Claim(ClaimTypes.Role, user.IdRoleNavigation.Libelle));
         }
 
         return principal;
